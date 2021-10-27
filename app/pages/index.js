3303d4922fec9a71e76672f15d9fcd0d31199c7b
mmonay24 <marabelle.monay@gmail.com>
2021-10-27 01:04:42 -0700

    Fixing the nav bar

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,14 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components';
+import React from 'react';
 import NavBar from '../comps/NavBar';
 
 export default function Home() {
   return (
     <div className={styles.container}>
         <NavBar></NavBar>
-       
-  
     </div>
   )
 }
