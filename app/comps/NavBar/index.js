743b493331027c5a7d245facc595a501561178f2
ChisakiNakamura <cnakamura1@my.bcit.ca>
2021-10-27 02:34:35 -0700

    initial commit

--- app/comps/NavBar/index.js
+++ app/comps/NavBar/index.js
@@ -1,20 +1,21 @@
+
 import Image from 'next/image'
 import styled from 'styled-components';
 
 const CtrlCont = styled.div`
-display: flex;
-flex-direction: column;
+display:flex;
+flex-direction:column;
 justify-content:center;
 align-items:center;
-width: 184px;
-height: 1024px;
-border-right-style: solid;
-border-right-color: #B1B1B1 ;
-border-width: 1px;
+width:184px;
+height:1024px;
+border-right-style:solid;
+border-right-color:#B1B1B1;
+border-width:1px;
 `
 const Logo = styled.img`
-width: 60px;
-height: 60px;
+width:50px;
+height:50px;
 margin-top:-150px;
 `
 const IconCont = styled.div`
@@ -27,24 +28,23 @@ height: 478px;
 margin: 50px;
 `
 const Icon = styled.img`
-width: 50px;
-height: 50px;
-margin: 15px;
+width: 30px;
+height: 30px;
+margin: 20px;
 `
 const NavBar = ({
 
 }) => {
     return <CtrlCont>
-                <Logo src="../../public/Rooma_Logo.svg"></Logo>
+                <Logo src="/Logo.png"/>
                 <IconCont>
-                    <Icon src="../../public/Home_Icon.svg"></Icon>
-                    <Icon src="../../public/Task_Icon.svg"></Icon>
-                    <Icon src="../../public/Chat_Icon.svg"></Icon>
-                    <Icon src="../../public/Members_Icon.svg"></Icon>
-                    <Icon src="../../public/Community_Icon.svg"></Icon>
+                    <Icon src="/Home_Icon.svg"/>
+                    <Icon src="/Task_Icon.svg"/>
+                    <Icon src="/Chat_Icon.svg"/>
+                    <Icon src="/Members_Icon.svg"/>
+                    <Icon src="/Community_Icon.svg"/>
                 </IconCont>
-                <Icon rc="../../public/Settings_Icon.svg"></Icon>
+                <Icon src="/Settings_Icon.svg"></Icon>
             </CtrlCont>   
 }
-
 export default NavBar;
