f59cf0bc1e94cab116a3b8fca1d2abd8c8ce9fab
ChisakiNakamura <cnakamura1@my.bcit.ca>
2021-11-02 01:50:44 -0700

    Toggle Button added to community

---
+++ app/comps/ToggleBtn/index.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import clsx from 'clsx';
+import { styled } from '@mui/system';
+import { useSwitch } from '@mui/core/SwitchUnstyled';
+
+const BasicSwitchRoot = styled('span')`
+  font-size: 0;
+  position: relative;
+  display: inline-block;
+  width: 43px;
+  height: 25px;
+  background-color: rgba(192, 182, 255, 0.5);
+  border-radius: 15px;
+  margin: 10px;
+  cursor: pointer;
+
+  &.Switch-disabled {
+    opacity: 0.4;
+    cursor: not-allowed;
+  }
+
+  &.Switch-checked {
+    background-color: rgba(192, 182, 255,1);
+  }
+`;
+
+const BasicSwitchInput = styled('input')`
+  cursor: inherit;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  z-index: 1;
+  margin: 0;
+`;
+
+const BasicSwitchThumb = styled('span')`
+  display: block;
+  width: 25px;
+  height: 25px;
+  border-radius: 25px;
+  background-color:#7751E8;
+  right: 3px;
+  position: relative;
+  transition: all 200ms ease;
+
+  &.Switch-focusVisible {
+    background-color: rgba(192, 182, 255,0.3);
+    box-shadow: 0 0 1px 8px rgba(0, 0, 0, 0.25);
+  }
+
+  &.Switch-checked {
+    left: 18px;
+    background-color:#7751E8;
+  }
+`;
+
+function BasicSwitch(props) {
+  const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
+
+  const stateClasses = {
+    'Switch-checked': checked,
+    'Switch-disabled': disabled,
+    'Switch-focusVisible': focusVisible,
+  };
+
+  return (
+    <BasicSwitchRoot className={clsx(stateClasses)}>
+      <BasicSwitchThumb className={clsx(stateClasses)} />
+      <BasicSwitchInput {...getInputProps()} aria-label="Demo switch" />
+    </BasicSwitchRoot>
+  );
+}
+
+export default function ToggleBtn() {
+  return (
+    <div>
+      <BasicSwitch />
+    </div>
+  );
+}
