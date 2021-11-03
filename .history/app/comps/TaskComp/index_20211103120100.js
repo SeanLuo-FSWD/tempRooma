60a641a7ab1fd02a8c8a953ec8e5c5f0bd23da0f
leahma1208 <leahma1208@gmail.com>
2021-11-03 14:43:28 -0700

    Add task component

---
+++ .history/app/comps/TaskComp/index_20211103120100.js
@@ -0,0 +1,25 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const Cont = styled.div`
+display:flex;
+flex-direction:row;
+width: 700px;
+height: 600px;
+box-shadow: 0px 4.0061163902282715px 47.07186508178711px 0px #0000001C;
+border-radius: 22px;
+
+
+`
+
+
+
+
+const TaskComp = ({
+
+})=>{
+  return<Cont>
+ 
+  </Cont>
+}
+export default TaskComp
