3cfa043c19adc7be4dd411c93bd9d481aca459be
ChisakiNakamura <cnakamura1@my.bcit.ca>
2021-11-09 01:17:58 -0800

    remider comp modified. need to add scroll function

--- app/comps/Reminder/index.js
+++ app/comps/Reminder/index.js
@@ -1,8 +1,12 @@
 import react from 'react';
+
 import styled from 'styled-components';
 import RemindContent from '../RemindContent';
-import Completed from '../Completed';
+import Completed_RemindContent from '../RemindContent/completed';
+//import Completed from '../Completed';
 import WeeklyRewards from '../WeeklyRewards';
+
+
 
 const Cont = styled.div`
 display:flex;
@@ -23,6 +27,11 @@ display:flex;
 flex-direction:column;
 flex-grow:1.5;
 `
+const HeadingCont = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+`
 const BotCont = styled.div`
 display:flex;
 flex-grow:1;
@@ -33,7 +42,7 @@ font-weight: 700;
 color: #181135;
 margin:0;
 margin-top:20px;
-margin-left:20px;
+margin-left:30px;
 margin-bottom:20px;
 `
 const DefMessage = styled.p`
@@ -48,7 +57,16 @@ height:45px;
 text-align:center;
 position:absolute;
 top:${props=>props.top};
-
+`
+const CompleteCont = styled.div`
+display:flex
+height:45px;
+margin-right:30px;
+margin-top:20px;
+`
+const Completed = styled.div`
+text-align:center;
+color:#7751E8;
 `
 const More = styled.div`
 width:685px;
@@ -64,41 +82,62 @@ const Icon = styled.img`
 
 const Reminder = ({
   heading="Today",
-  visibility="visible",
+  def_visibility="visible",
+  reminder_display="block",
+  reminder_completed_display="block",
   height="340px",
   top="435px",
-  title="more ",
-  complete_display="block",
-  more_display="flex",
-  complete_width="235px",
-  complete_height="47px",
-  complete_borderRadius="8px",
+  title_complete="complete ",
+  title_more="more ",
   rewards_display="block",
-  more_after_display="none",
+  checked="checked",
+  //complete_display="block",
+  //more_display="flex",
+  //complete_width="235px",
+  //complete_height="47px",
+  //complete_borderRadius="8px",
+  //more_after_display="none",
   onMoreClick=()=>{},
   onCompleteClick=()=>{},
   /* After expanding completed button on the top */
-  onCompleteClick_After=()=>{},
+  //onCompleteClick_After=()=>{},
 })=>{
 return<Cont>
   <TopCont>
   {/*Main task cont within CardCont */}
   <CardCont height={height}>
+  <scrollable-component>
+  <HeadingCont>
   <Heading className="ubuntu">{heading}</Heading>
+  <CompleteCont onClick={onCompleteClick} >
+    <Completed className="opensans">{title_complete}<Icon src="/down_arrow.png"/></Completed>
+    </CompleteCont>
+  </HeadingCont>
     <RemindContent
      bgcolor="rgba(240,199,137,30%)"
-     visibility="visible"
-     task_name="Task Name"
+     display={reminder_display}
+     task_name="On going Task Name"
      vlcolor="#F0C789"
      name="Name"
      date="5:00-7:00PM"
     />
-    <DefMessage className="opensans" visibility={visibility}>Nothing is scheduled for today.</DefMessage>
+    <Completed_RemindContent
+    bgcolor="rgba(240,199,137,15%)"
+    display={reminder_completed_display}
+    task_name="Completed Task Name"
+    vlcolor="rgba(241,178,82,25%)"
+    name="Name"
+    date="5:00-7:00PM"
+    checked={checked}
+    />
+    <DefMessage className="opensans" visibility={def_visibility}>Nothing is scheduled for today.</DefMessage>
     <MoreCont onClick={onMoreClick} top={top}>
-    <More className="opensans"><Divider/>{title}<Icon src="/down_arrow.png"/></More>
+    <More className="opensans"><Divider/>{title_more}<Icon src="/down_arrow.png"/></More>
     </MoreCont>
+    </scrollable-component>
   </CardCont>
   {/*completed cont */}
+  {/* 
   <Completed 
   display={complete_display}
   onCompleteClick={onCompleteClick}
@@ -108,10 +147,9 @@ return<Cont>
   more_display={more_display}
   more_after_display={more_after_display}
 
-  /* After expanding completed button on the top */
-  
   onClick={onCompleteClick_After}
   />
+  */}
   </TopCont>
   {/*WeeklyRewards cont */}
   <BotCont>
